44051a34b6e5dad53ab7f1848b000e7ed0b79849
Yustin Musidi <yustinmusidi@gmail.com>
2020-07-05 16:02:41 -0400

    Updates

---
+++ precache-manifest.dee6e14232382e83ceeb7f0762e0a32b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e53fa502106e3a5f938d4da00ec2ab",
+    "url": "/play-react/index.html"
+  },
+  {
+    "revision": "cc9b3f0fd7cd0a5fa1e3",
+    "url": "/play-react/static/css/main.652e6820.chunk.css"
+  },
+  {
+    "revision": "f31b70c5331eb58c83e7",
+    "url": "/play-react/static/js/2.b7125771.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/play-react/static/js/2.b7125771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc9b3f0fd7cd0a5fa1e3",
+    "url": "/play-react/static/js/main.e73c04e7.chunk.js"
+  },
+  {
+    "revision": "e6d76f7f6cedd679908c",
+    "url": "/play-react/static/js/runtime-main.0f3164f7.js"
+  }
+]);
